bfadfe20e203f8d45689b70eff3679d1a282dd2f
Oleg Bloshenkin <darelvc@gmail.com>
2016-03-11 09:46:37 +0200

    Rspec test errors with cancancan

--- spec/controllers/api/chats_controller_spec.rb
+++ spec/controllers/api/chats_controller_spec.rb
@@ -11,7 +11,9 @@ describe Api::ChatsController do
 
   it { should route(:put, '/api/chats/1').to(action: :update, id: 1) }
 
-  before { sign_in }
+  let(:current_user) { double }
+
+  before { sign_in current_user }
 
   describe '#index.json' do
     before { expect(subject.current_ability).to receive(:can?).with(:index, Chat).and_return(true) }
@@ -38,6 +40,17 @@ describe Api::ChatsController do
       { name: 'Test chat', description: 'Test this perfect chat' }
     end
 
+    let(:chat) { double }
+
+    before { expect(subject.current_ability).to receive(:can?).with(:create, Сhat).and_return(true) }
+
+    before { expect(Chat).to receive(:build).with(params).and_return(chat) }
+
+    before { expect(chat).to receive(:save!) }
+
+    before { post :create, id: 1, chat: params, format: :json }
+
+    it { should render_template :create }
   end
 end
 
